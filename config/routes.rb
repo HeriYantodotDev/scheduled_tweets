b69a4340a59a409a00b0ddc45826b51846122e1d
HeriYantodotDev <heri@HYA>
2023-02-04 15:10:03 +0700

    till part 9

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+
+  # Index
+  # get "/", to: "main#index" 
+  # or this could be like this 
+  root to: "main#index"
+
+  # get /about
+  get "about-us", to: "about#index", as: :about
+  get "main", to: "main#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
