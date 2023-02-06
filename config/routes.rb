a5f79121fc34c414cbe1a05df4cb9bd568261d82
HeriYantodotDev <heri@HYA>
2023-02-06 10:12:32 +0700

    setting up logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  get "sign_up", to: "registrations#new"
-  post "sign_up", to: "registrations#create"
-
-  # Index
+    # Index
   # get "/", to: "main#index" 
   # or this could be like this 
   root to: "main#index"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
 
   # get /about
   get "about-us", to: "about#index", as: :about
   get "main", to: "main#index"
 
-
+  #signout
+  delete "logout", to: "sessions#destroy"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
